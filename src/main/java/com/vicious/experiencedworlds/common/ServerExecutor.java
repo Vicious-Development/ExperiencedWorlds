c3d7180d7cd65d715fa1b22c7d0f55835f5d2dc5
Drathonix <52723768+Drathonix@users.noreply.github.com>
2022-10-24 12:36:47 -0400

    1.0.4 Final

--- src/main/java/com/vicious/experiencedworlds/common/ServerExecutor.java
+++ src/main/java/com/vicious/experiencedworlds/common/ServerExecutor.java
@@ -1,10 +1,10 @@
 package com.vicious.experiencedworlds.common;
 
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 public class ServerExecutor {
-    private static final ExecutorService executor = Executors.newFixedThreadPool(1);
+    private static final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
     public static void execute(Runnable run){
         executor.execute(run);
     }
